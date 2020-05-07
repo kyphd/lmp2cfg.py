b7170ea4e02327dd3f2cd0e340e46436ab1c1982
K.Y <20557345+kyphd@users.noreply.github.com>
2020-05-07 20:08:00 +0900

    v1

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # lmp2cfg.py
-Convert lammps dump file to atomeye cfg file.
+To convert lammps dump file to atomeye cfg file.
+
+Lammps: https://lammps.sandia.gov/
+
+Atomeye: http://li.mit.edu/Archive/Graphics/A/
+
+![lmp2cfg.py](https://github.com/kyphd/lmp2cfg.py/blob/master/images/lmp2cfg.png)
+
+## Table of Contents
+
+* [What is lmp2cfg.py?](#what-is-lmp2cfg.py)
+* [How to Use](#how-to-use)
+* [Contributing](#contributing)
+* [Authors](#authors)
+* [License](#license)
+
+## What is lmp2cfg.py?
+
+lmp2cfg.py is to convert lammps dump file to atomeye cfg file. 
+Lammps has "dump cfg" command, so this app is not important. 
+However, if you forget to dump cfg file, you can convert dump file to cfg file after MD calculation.
+Lammps tool also has lmp2cfg.f, lmp2cfg.f require to make an input file.
+lmp2cfg.py ask you the necessary parameter after execution and you don't need to prepare any files.
+
+## How to Use
+
+### 1. Execute lmp2cfg.py
+
+~~~
+$ python lmp2cfg.py <dump file>
+~~~
+
+Coordinate of each atom (\[x, y, z\] or \[xs, ys zs\]) and type arguments are required in dump file.
+
+Generally, "dump atom" command outputs all of them.
+
+### 2. You are asked the mass and element for each atom type in dump file.
+
+ex.
+
+~~~
+1 atom types are found. Input mass and element for each atom type.
+For #1
+ mass: 55.85
+ element: Fe
+~~~
+
+### 3. CFG file will be generated.
+
+## Contributing
+
+1. Fork this
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+## Authors
+
+* **K. Yabuuchi** 
+* See also the list of [contributors](https://github.com/kyphd/lmp2cfg.py/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ lmp2cfg.py
@@ -0,0 +1,123 @@
+# lmp2cfg.py v.1.0
+# convert lammps dump file to atomeye cfg file.
+# lammps: https://lammps.sandia.gov/
+# atomeye: http://li.mit.edu/Archive/Graphics/A/
+import sys
+
+args = sys.argv
+if len(args) == 1:
+    print("\x1b[31m error:\x1b[39m no input file.")
+    print("python lmp2cfg.py <dumpfile>")
+    sys.exit(1)
+
+inputfilename = args[1]
+outputfilename = inputfilename.split(".")[0] + ".cfg"
+
+numberOfAtoms = 0
+types = []
+cellSize = [0, 0, 0]
+atoms = []
+
+try:
+    with open(inputfilename) as inputfile:
+
+        print("Now reading {}".format(inputfilename))
+        flag = ""
+        xyzcol = [0, 0, 0]
+        typecol = 0
+        coodinatetype = ""
+
+        for line in [s.strip() for s in inputfile.readlines()]:
+
+            # get values
+            if flag == "number":
+                numberOfAtoms = int(line)
+                flag = ""
+
+            elif flag == "box-x":
+                cellSize[0] = float(line.split()[1]) - float(line.split()[0])
+                flag = "box-y"
+
+            elif flag == "box-y":
+                cellSize[1] = float(line.split()[1]) - float(line.split()[0])
+                flag = "box-z"
+
+            elif flag == "box-z":
+                cellSize[2] = float(line.split()[1]) - float(line.split()[0])
+                flag = ""
+
+            elif flag == "atoms":
+                cols = line.split()
+                if coodinatetype == "unscaled":
+                    x = float(cols[xyzcol[0]]) / cellSize[0]
+                    y = float(cols[xyzcol[1]]) / cellSize[1]
+                    z = float(cols[xyzcol[2]]) / cellSize[2]
+                elif coodinatetype == "scaled":
+                    x = float(cols[xyzcol[0]])
+                    y = float(cols[xyzcol[1]])
+                    z = float(cols[xyzcol[2]])
+
+                atoms.append([int(cols[typecol]), x, y, z])
+                if not int(cols[typecol]) in types:
+                    types.append(int(cols[typecol]))
+
+            # set flag
+            if line == "ITEM: NUMBER OF ATOMS":
+                flag = "number"
+            elif line[0:16] == "ITEM: BOX BOUNDS":
+                flag = "box-x"
+            elif line[0:11] == "ITEM: ATOMS":
+                flag = "atoms"
+                cols = line.split()
+                if "type" in cols and "x" in cols and "y" in cols and "z" in cols:
+                    typecol = cols.index("type") - 2
+                    xyzcol[0] = cols.index("x") - 2
+                    xyzcol[1] = cols.index("y") - 2
+                    xyzcol[2] = cols.index("z") - 2
+                    coodinatetype = "unscaled"
+                elif "type" in cols and "xs" in cols and "ys" in cols and "zs" in cols:
+                    typecol = cols.index("type") - 2
+                    xyzcol[0] = cols.index("xs") - 2
+                    xyzcol[1] = cols.index("ys") - 2
+                    xyzcol[2] = cols.index("zs") - 2
+                    coodinatetype = "scaled"
+                else:
+                    print("Error: lmt2cfg.py requires type and coordinate (x y z or xs ys zs) args in dump file.")
+                    sys.exit(1)
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+# ask atom mass and element for each atom type
+types.sort()
+masses = {}
+elements = {}
+print("{} atom types are found. Input mass and element for each atom type.".format(len(types)))
+for atomtype in types:
+    print("For #{}".format(atomtype))
+    print(" mass: ", end='')
+    masses[atomtype] = input()
+    print(" element: ", end='')
+    elements[atomtype] = input()
+
+# output cfg file
+try:
+    with open(outputfilename, "w") as out:
+        out.write("Number of particles = {}\n".format(numberOfAtoms))
+        out.write("A = 1 Angstrom (basic length-scale)\n")
+        out.write("H0(1,1) = {} A\n".format(cellSize[0]))
+        out.write("H0(1,2) = 0 A\n")
+        out.write("H0(1,3) = 0 A\n")
+        out.write("H0(2,1) = 0 A\n")
+        out.write("H0(2,2) = {} A\n".format(cellSize[1]))
+        out.write("H0(2,3) = 0 A\n")
+        out.write("H0(3,1) = 0 A\n")
+        out.write("H0(3,2) = 0 A\n")
+        out.write("H0(3,3) = {} A\n".format(cellSize[2]))
+        for atom in atoms:
+            out.write("{} {} {} {} {} 0 0 0\n".format(masses[atom[0]], elements[atom[0]], atom[1], atom[2], atom[3]))
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
